f1b5e0faf34251caabab1ec7b8471e8f8db62038
Tera <a>
2020-06-14 23:54:24 +0900

    a

---
+++ Source/Statue/StatueGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StatueGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStatueGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStatueGameMode();
+};
+
+
+
